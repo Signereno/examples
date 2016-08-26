c33c2903b2e0d7447f28952632f951ad5c335629
steinolav.davidsen <stein@unipluss.no>
2016-08-26 18:19:26 +0200

    updated offline sign example

--- SignereOfflineTestWithClient/SignereOfflineTestWithClient/Program.cs
+++ SignereOfflineTestWithClient/SignereOfflineTestWithClient/Program.cs
@@ -12,8 +12,8 @@ namespace SignereOfflineTestWithClient
     {
         static void Main(string[] args)
         {
-            string apiID = "80da60ca-09bc-4b77-adf4-a66e009efe9b";
-            string primaryApiKey = "L2tBTDBYcm11UGNYc0oxUHpNUWluQT09LEdHQlJ0R3pKU1Nuem9UNkxDSk1jcFNvN1kvT2dnV1VGSUFiY2JQQzVhQVE9";
+            string apiID = "Enter your API ID here"
+            string primaryApiKey = "Enter your primary API key here"
             Client client = new Client("https://testapi.signere.no", new Guid(apiID), primaryApiKey,false,true,true);
             NewDocument document = new NewDocument()
             {
@@ -27,18 +27,18 @@ namespace SignereOfflineTestWithClient
                         new SigneeRef()
                         {
                             ExternalSigneeId = "12345",
-                            Email = "steinolavdavidsen@gmail.com",
-                            Mobile = "95767053",
-                            FirstName = "Stein-Olav",
-                            LastName = "Davidsen"
+                            Email = "enter the recipients' email address here",
+                            Mobile = "enter the recipients' mobile number here",
+                            FirstName = "Testmann",
+                            LastName = "Testesen"
                         }
                     },
                 CreatePADES = true,
-                MessageEmail = "steinolavdavidsen@gmail.com",
-                MessageSms = "95767053",
-                SenderEmail = "stein@signere.no",
-                SenderMobile = "95767053",
-                TopicEmail = "testsignering",
+                MessageEmail = "enter the email text here",
+                MessageSms = "enter the sms text here",
+                SenderEmail = "enter the sender's email address here",
+                SenderMobile = "enter the sender's mobile number here",
+                TopicEmail = "enter the topic of the email here",
                 ExternalDocumentId = "123435",
                 SignDeadline = DateTime.Today.AddDays(3),
                 DoNotNotifySigneeRefs = false,
@@ -51,8 +51,8 @@ namespace SignereOfflineTestWithClient
                 NotifySigneerefsWhenSignedEmail = false,
                 NotifySigneerefsWhenSignedSMS = false,
                 Private = false,
-                ReceiptMessageEmail = "stein@signere.no",
-                ReceiptTopicEmail = "Test",
+                ReceiptMessageEmail = "enter the text of the receipt email here",
+                ReceiptTopicEmail = "enter the topic of the receipt email here",
                 ShowOnSigneesPersonalSite = true,
                 CreatedByApplication = "test",
                 ConvertToPDFA2b = false,
@@ -64,11 +64,11 @@ namespace SignereOfflineTestWithClient
             {
                 var documentJobId = client.CreateDocumentJob(new DocumentJob()
                 {
-                    Contact_Email = "stein@signere.no",
-                    Contact_Mobile = "95767053",
-                    Contact_Phone = "95767053",
-                    Contact_Name = "Stein-Olav Davidsen",
-                    Contact_Url = "https://www.signere.no"
+                    Contact_Email = "enter contact email address here",
+                    Contact_Mobile = "enter contact mobile number here",
+                    Contact_Phone = "enter contact phone number here",
+                    Contact_Name = "enter contact person's name here",
+                    Contact_Url = "enter contact website here"
                 });
                 client.CreateDocumentInJob(document, documentJobId, "Testdokument.pdf");
             }
